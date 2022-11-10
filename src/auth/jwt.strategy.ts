eb5ac7bfa29f71be4c972c0c2daa8aff2f862df3
chanjeff <jeffchan142@gmail.com>
2022-11-10 23:20:58 +0800

    replace simple user object with object model for jwt

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,12 +1,17 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from 'src/interfaces/jwt-payload.interface';
+import { User } from 'src/models/user.model';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,7 +19,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: JwtPayload) {
-    return { userId: payload.userId, username: payload.username };
+  async validate(payload: JwtPayload): Promise<User> {
+    const user = await this.usersService.getUserById(payload.userId);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
