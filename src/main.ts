673466893873df248b90ea62404dc1cade9a0705
chanjeff <jeffchan142@gmail.com>
2022-11-14 13:29:17 +0800

    add PORT to .env

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -8,7 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT') || 3000);
 
   if (module.hot) {
     module.hot.accept();
